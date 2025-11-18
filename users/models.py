fb0781f9e73e900b0aaa378e0d525d2e9e8bfb1c
Andrei Makarov <aomak@yandex.ru>
2025-11-19 00:04:41 +0300

    сделал модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,4 +1,21 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True, verbose_name="Почта")
+    phone_number = models.CharField(
+        max_length=15, blank=True, null=True, verbose_name="Номер телефона"
+    )
+    city = models.CharField(max_length=50, verbose_name='Город', null=True, blank=True)
+    avatar = models.ImageField(
+        upload_to="avatars/", blank=True, null=True, verbose_name="Аватар"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "username",
+    ]
+
+    def __str__(self):
+        return self.email
