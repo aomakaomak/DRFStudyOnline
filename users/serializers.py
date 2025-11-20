b9c27d6c4d60644297c14c2dc4607ea328c496cf
Andrei Makarov <aomak@yandex.ru>
2025-11-20 17:21:41 +0300

    Для профиля пользователя сделан вывод истории платежей

--- users/serializers.py
+++ users/serializers.py
@@ -3,15 +3,18 @@ from rest_framework import serializers
 from users.models import User, Payment
 
 
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+
 class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
 
     class Meta:
         model = User
         fields = '__all__'
 
 
-class PaymentSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Payment
-        fields = '__all__'
