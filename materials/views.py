c7109244d13677e6f15a814e5f545d0f99c573c8
Andrei Makarov <aomak@yandex.ru>
2025-11-20 17:12:57 +0300

    настроена фильрация

--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets, generics
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter
+
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 from users.models import User, Payment
 from users.serializers import UserSerializer, PaymentSerializer
@@ -11,3 +13,6 @@ class UserViewSet(viewsets.ModelViewSet):
 class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'lesson', 'payment_method')
+    ordering_fields = ('payment_date',)
