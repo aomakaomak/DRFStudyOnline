9d091b75fe3ed1aced9f4de0e75a2ec635d54c36
Andrei Makarov <aomak@yandex.ru>
2025-11-20 15:54:25 +0300

    добавлено поле количество уроков для курса

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,10 +4,14 @@ from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lessons_count(self, instance):
+        return instance.lessons.count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
