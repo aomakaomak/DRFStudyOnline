d164bdecbde9dd2fee656a64b48bf64d26aa45f8
Andrei Makarov <aomak@yandex.ru>
2025-11-20 16:19:45 +0300

    добавлена модель платежи

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from materials.models import Course, Lesson
+
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name="Почта")
@@ -19,3 +21,32 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+
+class Payment(models.Model):
+    CASH = 'cash'
+    TRANSFER = 'transfer'
+
+    PAYMENT_METHOD_CHOICES = [
+        (CASH, 'наличные'),
+        (TRANSFER, 'банковский перевод'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payments', verbose_name='Пользователь')
+    payment_date = models.DateField(verbose_name='Дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='payments', verbose_name='Оплаченный курс', blank=True, null=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='payments', verbose_name='Оплаченный урок', blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name='Метод оплаты')
+
+    def __str__(self):
+        return f'Платеж #{self.id} от {self.user} на сумму {self.amount}'
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
+
