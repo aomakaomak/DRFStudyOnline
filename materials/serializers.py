d671115973b5a8c34930c9a90b05c1a7969657bc
Andrei Makarov <aomak@yandex.ru>
2025-11-24 18:48:22 +0300

    задание 1 готово

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from materials.models import Course, Lesson
+from materials.validators import LinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -8,6 +9,10 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+        validators = [
+            LinkValidator(field='link')
+        ]
 
 
 class CourseSerializer(serializers.ModelSerializer):
